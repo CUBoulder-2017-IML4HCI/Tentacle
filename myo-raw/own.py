45cbe2ff3670ca3119f518aaa22f25f82c84eed0
ByronBecker <byron.becker@colorado.edu>
2017-04-30 15:58:40 -0600

    modified own.py, getting emg data successfully to port 12000

--- myo-raw/own.py
+++ myo-raw/own.py
@@ -8,27 +8,40 @@ import OSC
 wek = OSC.OSCClient()
 wek.connect(('127.0.0.1', 6448))
 
-def send(data): 
+tenta = OSC.OSCClient()
+tenta.connect(('127.0.0.1', 12000))
+
+def send(data, dest, address): 
     oscmsg = OSC.OSCMessage()
-    oscmsg.setAddress("/wek/inputs")
-    for i,elem in enumerate(data):
+    oscmsg.setAddress(address)
+    for elem in data:
             oscmsg.append(float(elem))
     
-    wek.send(oscmsg)
+    dest.send(oscmsg)
     print(oscmsg)
-    #print oscmsg[1:]
     print(data)
 
 def imu(quat,acc,gyro):
-    	#print(quat,acc,gyro)
-    	print(quat,acc,gyro)
-    	send(quat)
+    print(quat,acc,gyro)
+    print ("quat: ", quat, " type: ", type(quat))
+    send(quat, wek, "/wek/inputs")
+
+def pose(p):
+    print(p) 
+    
+def emg(emg, moving): 
+   emgdat = (sum(emg)/float(len(emg)),)
+   print ("emgdat: ", emgdat, " type: ", type(emgdat))
+   send(emgdat, tenta, "/tenta_emg")
+   
 
 if __name__ == '__main__':
     
     m = MyoRaw()
     #m.add_imu_handler(print)    
-    m.add_imu_handler(imu)
+    #m.add_imu_handler(imu)
+    #m.add_pose_handler(pose)
+    m.add_emg_handler(emg)
     m.connect()
     while True:
         m.run()
